1b622911a2997bce9b903eb58eaadea9da71917c
Richard Reed <reedqa@gmail.com>
2025-02-04 13:50:35 -0600

    stubbed out test search

---
+++ tests/test_search.py
@@ -0,0 +1,18 @@
+"""
+These tests cover DuckDuckGo searches.
+"""
+
+def test_basic_duckduckgo_search(browser):
+
+    # Given the DuckDuckGo home page is displayed
+    
+    # When the user searches for "panda"
+
+    # Then the search result title contains "panda"
+
+    # And the search result query is "panda"
+
+    # And the search result links pertain to "panda"
+
+    raise Exception("Incomplete Test")
+
