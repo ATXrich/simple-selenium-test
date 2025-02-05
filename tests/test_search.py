99e9902e2f2fb2c6f68ced86cbcbf16336efa093
Richard Reed <reedqa@gmail.com>
2025-02-05 13:18:40 -0600

    parametrize phrases

--- tests/test_search.py
+++ tests/test_search.py
@@ -2,30 +2,32 @@
 These tests cover DuckDuckGo searches.
 """
 
+import pytest
+
 from pages.result import DuckDuckGoResultPage
 from pages.search import DuckDuckGoSearchPage
 
 
-def test_basic_duckduckgo_search(browser):
+@pytest.mark.parametrize('phrase', ['panda', 'python', 'polar bear'])
+def test_basic_duckduckgo_search(browser, phrase):
     search_page = DuckDuckGoSearchPage(browser)
     result_page = DuckDuckGoResultPage(browser)
-    PHRASE = 'panda'
 
     # Given the DuckDuckGo home page is displayed
     search_page.load()
 
-    # When the user searches for "panda"
-    search_page.search(PHRASE)
+    # When the user searches for phrase
+    search_page.search(phrase)
 
-     # And the search result query is "panda"
-    assert PHRASE == result_page.search_input_values()
+     # And the search result query is phrase
+    assert phrase == result_page.search_input_values()
 
-    # And the search result links pertain to "panda"
+    # And the search result links pertain to phrase
     for title in result_page.result_link_titles():
-        assert PHRASE.lower() in title.lower()
+        assert phrase.lower() in title.lower()
 
-    # Then the search result title contains "panda"
-    assert PHRASE in result_page.title()
+    # Then the search result title contains phrase
+    assert phrase in result_page.title()
 
     # This exception can be removed after the test is fully implemented.
     # raise Exception("Incomplete Test")
